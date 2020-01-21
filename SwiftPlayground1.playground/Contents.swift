a28731458b937e4c685cc563814a64a79fee970a
Rkozuma <54559054+Rkozuma@users.noreply.github.com>
2020-01-21 10:32:01 -1000

    Complete Step 8

--- SwiftPlayground1.playground/Contents.swift
+++ SwiftPlayground1.playground/Contents.swift
@@ -1,14 +1,23 @@
 import UIKit
 
-var str = "Hello, playground"
+//var str = "Hello, playground"
 
 //var MyFirstVariable: String
 
-var MyFirstVariable = "I've created my first string!"
+//var MyFirstVariable = "I've created my first string!"
 
 //Declare and assign variables in a single line of code
-var MySecondVariable = "and look I added two strings together!"
+//var MySecondVariable = "and look I added two strings together!"
 
-var MyThirdVariable = MyFirstVariable + MySecondVariable
+//var MyThirdVariable = MyFirstVariable + MySecondVariable
 
-print(MyThirdVariable)
+//print(MyThirdVariable)
+
+
+var Var1 = "Hello "
+var Var2 = "My "
+var Var3 = "Name"
+
+var Var4 = Var1 + Var2 + Var3
+
+print(Var4)
